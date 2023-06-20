b6dfaa4bf92ecbe01ed193d50a9bea8974144489
Leo <leoliu5550@gmail.com>
2023-06-20 15:30:10 +0800

    OK

---
+++ Crank_Nicolson_Method.py
@@ -0,0 +1,5 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import math
--- temp.py
+++ temp.py
@@ -4,78 +4,27 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 
-N = 1000
-size = 5
-TOL = 1e-6
 
-def create_data(size):
-    A_matrix = np.zeros((size,size))
-    for i in range(size):
-        for j in range(size):
-            if i == j:
-                A_matrix[i][j] = 2*(i+1)
-            if j == (i+2) or j == (i-2):
-                A_matrix[i][j] = 0.5*(i+1)
-            if j == (i+4) or j == (i-4):
-                A_matrix[i][j] = 0.25*(i+1)    
-    B_vector = np.array([np.pi for i in range(size)])
-                
-    return A_matrix,B_vector
-
-
-def escape(old_x,new_x,TOL):
-    if np.max(np.abs(old_x - new_x)) < TOL:
-        return True,np.max(np.abs(old_x - new_x))
-    return False,np.max(np.abs(old_x - new_x))
-
-
-def draw(loss,title=None):
-    ite = [i for i in range(1,len(loss)+1,1)]
-    fig = plt.figure(figsize = (15,8))
-    ax = fig.add_subplot(111)
-    if title is not None:
-        ax.set_title(title)
-    ax.xaxis.set_major_locator(MultipleLocator(1))
-    ax.yaxis.set_major_locator(MultipleLocator(0.1))
-    ax.plot(ite,loss,lw = 1,zorder = 10,label = 'norm')
-    ax.grid(linestyle="--",linewidth = 0.5,zorder = 0)#pair0
-    ax.legend()
+def exact_soluation(x_range, t_range):
+    return np.exp(-(np.pi**(2))*t_range/4)*t_range*np.sin(np.pi*x_range/2)
     
-Test_matrix = np.array(
-    [
-    [10,-1,2,0],
-    [-1,11,-1,3],
-    [2,-1,10,-1],
-    [0,3,-1,8]]
-)
-tb_matrix = np.array(
-    [6,25,-11,15]
-)
-
-def SOR(A_matrix,B_matrix,w,test =False):
-    k = 1
-    n = A_matrix.shape[0]
-    loss = []
-    init_x = np.zeros_like(B_matrix,dtype="float64")
-    x = copy.deepcopy(init_x)
-    while(k<N):
-        x_p = copy.deepcopy(x)
-        if test:
-            print(x)
-        for i in range(n):
-            summ = 0.0
-            for j in range(n):
-                if j==i:
-                    continue
-                summ += A_matrix[i][j]*x_p[j]
-            x[i] = (-summ + B_matrix[i])/A_matrix[i][i]
-            x[i] = x[i] * w + x_p[i]*(1-w)
-        esp = escape(x_p,x,TOL)[0]
-        
-        loss.append(escape(x_p,x,TOL)[1])
-        if esp:
-            break
-        k+=1
-    return x,loss,k
 
 
+x = np.linspace(0, 2, num=10)
+tim = np.linspace(0,2,num =90)
+# print(x)
+# print(exact_soluation(x, tim))
+
+x,t = np.meshgrid(x,tim)
+u = exact_soluation(x,t)
+
+ax = plt.axes(projection='3d')
+# ax.plot_surface(x,t,u) 
+ax.plot_wireframe(x,t,u,color='b') 
+ax.plot_wireframe(x,t,u**(3),color='r') 
+ax.set_xlabel('x')
+ax.set_ylabel('t')
+ax.set_zlabel('u')
+# ax.set_box_aspect((1, 1, 0.5))
+# plt.title('Axes3D Plot Surface')
+plt.show()
